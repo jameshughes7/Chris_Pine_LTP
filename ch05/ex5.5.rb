0adc6245d9c2ab515a04cb59eea3d7941f2c3ce2
James Hughes <hugjimbo77@gmail.com>
2017-08-07 16:26:03 +0100

    Chapter 5

---
+++ ch05/ex5.5.rb
@@ -0,0 +1,8 @@
+puts 'Hello! What\'s your first name?'
+first_name = gets.chomp
+puts 'And what is your second name?'
+middle_name = gets.chomp
+puts 'Finally, what is your last name?'
+last_name = gets.chomp
+puts ''
+puts "Your full name is #{first_name} #{middle_name} #{last_name}."
---
+++ ch05/ex5.6.rb
@@ -0,0 +1,3 @@
+puts 'What is your favourite number?'
+favourite_number = gets.chomp.to_i
+puts "No worries, but #{favourite_number + 1} has got to be the best number, right?"
